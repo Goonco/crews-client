f066ccc60c3942d5b3fb54481d3e8cabe60f124d
Goonco <s07019@naver.com>
2024-02-15 15:54:48 +0900

    :bug: MSW catch all로 인한 hot reload 문제 해결

--- src/apis/api/signin.js
+++ src/apis/api/signin.js
@@ -6,19 +6,19 @@ export const SIGNIN_REQUEST = {
   signin: SIGNIN_DEFAULT + '',
 };
 
-export const signInAPI = {
-  signIn: async (inputs) => {
-    const { id, pw } = { ...inputs };
-    return await baseInstance.post(
-      SIGNIN_REQUEST.signin,
-      JSON.stringify({ id, pw }),
-      {
-        headers: { 'Content-Type': 'application/json' },
-        withCredentials: true,
-      }
-    );
-  },
-};
+// export const signInAPI = {
+//   signIn: async (inputs) => {
+//     const { id, pw } = { ...inputs };
+//     return await baseInstance.post(
+//       SIGNIN_REQUEST.signin,
+//       JSON.stringify({ id, pw }),
+//       {
+//         headers: { 'Content-Type': 'application/json' },
+//         withCredentials: true,
+//       }
+//     );
+//   },
+// };
 
 export const signInApi = {
   endpoint: {
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -41,80 +41,80 @@ export const handlers = [
   ),
 
   // *. Refresh
-  http.get('/refresh', async ({ cookies }) => {
-    const { refreshTok } = { ...cookies };
-    const { status, msg, payload } = { ...(await verifyToken(refreshTok)) };
+  // http.get('/refresh', async ({ cookies }) => {
+  //   const { refreshTok } = { ...cookies };
+  //   const { status, msg, payload } = { ...(await verifyToken(refreshTok)) };
 
-    if (status === 200) {
-      const accessToken = await createAccess({ id: payload.id });
-      return HttpResponse.json({ accessToken });
-    } else return new HttpResponse(msg, { status });
-  }),
+  //   if (status === 200) {
+  //     const accessToken = await createAccess({ id: payload.id });
+  //     return HttpResponse.json({ accessToken });
+  //   } else return new HttpResponse(msg, { status });
+  // }),
 
-  // 1. Sign In Page
-  http.post(SIGNIN_REQUEST.signin, async ({ request }) => {
-    const { id, pw } = { ...(await request.json()) };
-    const accessTok = await createAccess({ id });
-    const refreshTok = await createRefresh({ id });
+  // // 1. Sign In Page
+  // http.post(SIGNIN_REQUEST.signin, async ({ request }) => {
+  //   const { id, pw } = { ...(await request.json()) };
+  //   const accessTok = await createAccess({ id });
+  //   const refreshTok = await createRefresh({ id });
 
-    if (id === 'admin' && pw === '1234') {
-      return new HttpResponse(
-        JSON.stringify({ accessToken: accessTok, roles: ['leader'] }),
-        {
-          status: 200,
-          headers: {
-            'Set-Cookie': `refreshTok=${refreshTok}; Max-Age=3600;`,
-          },
-        }
-      );
-      // } else if (id === 'user' && pw === '1234') {
-      //   return new HttpResponse(
-      //     JSON.stringify({ accessToken: accessTok, roles: ['member'] }),
-      //     {
-      //       status: 200,
-      //       headers: { 'Set-Cookie': refreshTok },
-      //     }
-      //   );
-    } else return new HttpResponse(null, { status: 404 });
-  }),
+  //   if (id === 'admin' && pw === '1234') {
+  //     return new HttpResponse(
+  //       JSON.stringify({ accessToken: accessTok, roles: ['leader'] }),
+  //       {
+  //         status: 200,
+  //         headers: {
+  //           'Set-Cookie': `refreshTok=${refreshTok}; Max-Age=3600;`,
+  //         },
+  //       }
+  //     );
+  //     // } else if (id === 'user' && pw === '1234') {
+  //     //   return new HttpResponse(
+  //     //     JSON.stringify({ accessToken: accessTok, roles: ['member'] }),
+  //     //     {
+  //     //       status: 200,
+  //     //       headers: { 'Set-Cookie': refreshTok },
+  //     //     }
+  //     //   );
+  //   } else return new HttpResponse(null, { status: 404 });
+  // }),
 
-  // 8. 지원서 작성 페이지
-  http.get(WRITE_FORM_REQUEST.sectionData, () => {
-    return Response.json(DUMMY_SECTION_DATA);
-  }),
+  // // 8. 지원서 작성 페이지
+  // http.get(WRITE_FORM_REQUEST.sectionData, () => {
+  //   return Response.json(DUMMY_SECTION_DATA);
+  // }),
 
-  http.get(WRITE_FORM_REQUEST.questionData, () => {
-    return Response.json(DUMMY_QUESTION_DATA);
-  }),
+  // http.get(WRITE_FORM_REQUEST.questionData, () => {
+  //   return Response.json(DUMMY_QUESTION_DATA);
+  // }),
 
-  // 9. 지원서 평가 페이지
-  http.get(EVALUATE_FORM_REQUEST.recruitmentName(), async ({ params }) => {
-    const dummyData = DUMMY_RECRUITMENT_NAME(params.formId);
+  // // 9. 지원서 평가 페이지
+  // http.get(EVALUATE_FORM_REQUEST.recruitmentName(), async ({ params }) => {
+  //   const dummyData = DUMMY_RECRUITMENT_NAME(params.formId);
 
-    if (dummyData) return HttpResponse.json(dummyData);
-    else throw new Error();
-  }),
+  //   if (dummyData) return HttpResponse.json(dummyData);
+  //   else throw new Error();
+  // }),
 
-  http.get(
-    EVALUATE_FORM_REQUEST.applicantList(),
-    async ({ params, request }) => {
-      try {
-        const dummyData = DUMMY_APPLICANT_LIST(params.formId);
+  // http.get(
+  //   EVALUATE_FORM_REQUEST.applicantList(),
+  //   async ({ params, request }) => {
+  //     try {
+  //       const dummyData = DUMMY_APPLICANT_LIST(params.formId);
 
-        if (!dummyData) return HttpResponse('Not Found', { status: 404 });
-        if (!request.headers?.get('Authorization'))
-          return new HttpResponse('No Access Token', { status: 401 });
+  //       if (!dummyData) return HttpResponse('Not Found', { status: 404 });
+  //       if (!request.headers?.get('Authorization'))
+  //         return new HttpResponse('No Access Token', { status: 401 });
 
-        const token = request.headers?.get('Authorization').split(' ')[1];
-        const { status, msg } = { ...(await verifyToken(token)) };
+  //       const token = request.headers?.get('Authorization').split(' ')[1];
+  //       const { status, msg } = { ...(await verifyToken(token)) };
 
-        if (status === 200) return HttpResponse.json(dummyData);
-        else return new HttpResponse(msg, { status });
-      } catch (e) {
-        console.log(e, '****************************');
-      }
-    }
-  ),
+  //       if (status === 200) return HttpResponse.json(dummyData);
+  //       else return new HttpResponse(msg, { status });
+  //     } catch (e) {
+  //       console.log(e, '****************************');
+  //     }
+  //   }
+  // ),
 
   // 9*. 지원서 평가 디테일 페이지
 
@@ -129,6 +129,6 @@ export const handlers = [
   // }),
 
   // Catch All
-  http.get('/*', () => HttpResponse.error()),
-  http.post('/*', () => HttpResponse.error()),
+  // http.get('/*', () => new HttpResponse(null, { status: 200 })),
+  // http.post('/*', () => new HttpResponse(null, { status: 200 })),
 ];
